00ed19382bba5ad8d73143d9a52391953c86238f
SkyJohn233 <skyjohn@pku.edu.cn>
2021-04-30 23:25:24 +0800

    no comment

---
+++ Auth/src/main/java/AuthServer/total.java
@@ -0,0 +1,12 @@
+package AuthServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class total {
+
+    public static void main(String[] args){
+        SpringApplication.run(total.class,args);
+    }
+}
